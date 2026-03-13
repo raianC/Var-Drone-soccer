6339dc9d2b6dfd473cec349c31fccd21018dff39
EmaVerseux <ema.verseux@gmail.com>
2026-03-13 11:59:10 +0100

    Ajout bouton pour quitter plein écran + affichage décompte temps match sur la fenêtre commune

--- fichier_test.py
+++ fichier_test.py
@@ -1,19 +1,60 @@
 from PySide6.QtWidgets import * #importe tous les widgets 
 
+
+
+
+
+
 class Timer(QWidget):
     
+    
+    def __init__(self, duree_match, chrono, timer_match):
+        super().__init__()
+
+        self.duree_match = duree_match
+        self.chrono = chrono
+        self.timer_match = timer_match
+
+
     def update_timer_match(self):
-    temps_match -= 1
-    minutes = temps_match//60 # // division entière, sans virgule
-    secondes = temps_match % 60
-    chrono.setText(f"{minutes}:{secondes}")
-    if temps_match == 0:   # si 3 minutes atteintes
-        timer_match.stop()   # on arrête le timer
-        print("Temps match écoulé")
-        update_timer_penalite()
-        timer_penalite.start(1000)
+        self.duree_match -= 1
+        minutes = self.duree_match//60 # // division entière, sans virgule
+        secondes = self.duree_match % 60
+        self.chrono.setStyleSheet("color:white; font-size:40px; border:2px solid white;")
+        self.chrono.setText(f"{minutes}:{secondes}")
+        
+        print("Tic")
+        if self.duree_match == 0:   # si 3 minutes atteintes
+            self.timer_match.stop()   # on arrête le timer
+            print("Temps match écoulé")
+            #update_timer_penalite()
+            # timer_penalite.start(1000)
 
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #def update_timer_penalite():
 #    chrono.setStyleSheet("color: red; font-size: 40px;")
--- main.py
+++ main.py
@@ -5,20 +5,17 @@ from PySide6.QtWidgets import * #importe tous les widgets
 from PySide6.QtCore import QTimer, Qt #nécessaire Qtimer
 
 
+
+
+duree_match=20
+
 app = QApplication(sys.argv)
-interface=QWidget()                    #création fen^tre vide
+interface=QWidget()                    #création fenêtre vide
 interface.setWindowTitle("Interface")     # Titre de la fenêtre
-interface.resize(400, 400)             # Taille de la fenêtre (largeur x hauteur)
-
-interface.show()
+#interface.resize(400, 400)             # Taille de la fenêtre (largeur x hauteur)
 
 
-timer_match = QTimer()           # Création d'un QTimer
-timer_match.timeout.connect(update_timer_match) # à chaque "tic" d'horloge exécution de la fonction update_timer
-timer_match.start(1000)
 
-#timer_penalite = QTimer()           # Création d'un QTimer
-#timer_penalite.timeout.connect(Tupdate_timer_penalite) # à chaque "tic" d'horloge exécution de la fonction update_timer
 
 
 
@@ -27,24 +24,27 @@ chrono.setAlignment(Qt.AlignCenter)
 chrono.resize(400, 400)
 chrono.setStyleSheet("color:white; font-size: 40px;")  # augmente la taille d'écriture du chrono                      
 
+bouton_quitter = QPushButton("Quitter", interface)
+
+
+interface.showFullScreen() #montre la fenêtre (Attention, montrer après avoir créé les labels)
+
+largeur_ecran = interface.width()
+hauteur_ecran = interface.height()
+
+
+bouton_quitter.setGeometry(largeur_ecran-60,10, 55, 55)  # setGeometry(x, y, width, height)
+bouton_quitter.clicked.connect(interface.close)
+
+
+timer_match = QTimer() # Création d'un QTimer
+timer = Timer(duree_match, chrono, timer_match)
 
 
 
+timer_match.timeout.connect(timer.update_timer_match) # à chaque "tic" d'horloge exécution de la fonction update_timer
+timer_match.start(1000)
 
-
-
-
-
-
-
-
-
-
-
-
-
-timer=Timer()
-timer.validation_communication_main_fichier_test()
 
 
 sys.exit(app.exec())
