031397a7ae83c7c32e5123110ee6766cd0a429be
EmaVerseux <ema.verseux@gmail.com>
2026-05-14 19:47:09 +0200

    enchainement 2 ou 3 sets + pénalités equipe1 + pénalités équipe2

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ score2 = 0
 score1_total=0
 score2_total=0
 
-duree_penalite_accordee_equipe1 = 10
-duree_penalite_accordee_equipe2 = 10
-duree_match = 20
+duree_penalite_accordee_equipe1 = 0
+duree_penalite_accordee_equipe2 = 0
+duree_match = 8
 
 nombre_sets = 0
 numero_set = 1
@@ -24,10 +24,68 @@ app = QApplication(sys.argv)
 interface = QWidget()
 interface.setWindowTitle("Interface")
 
-
 def start_set():
+    global duree_penalite_accordee_equipe1
+    global duree_penalite_accordee_equipe2
+
+    duree_penalite_accordee_equipe1=0  # pénalités remises à 0 pour chaque début de match
+    duree_penalite_accordee_equipe2=0
+
     timer_match.start(1000)
-    bouton_start.hide()
+    bouton_start_set.hide()
+    timer_match.timeout.connect(timer.update_timer_match)
+
+
+def fin_set():
+    timer_match.timeout.disconnect(timer.update_timer_match)
+    global numero_set
+    numero_set += 1
+    timer.reset_match()
+
+
+    #juste pour les tests avant la partie de Dorian
+    global duree_penalite_accordee_equipe1
+    duree_penalite_accordee_equipe1=5
+    global duree_penalite_accordee_equipe2
+    duree_penalite_accordee_equipe2=5
+
+
+
+    if duree_penalite_accordee_equipe1 > 0:
+        bouton_start_penalite_equipe1.show()
+    else:
+        fin_penalite_accordee_equipe1()
+
+def start_penalite_equipe1():
+    
+    timer.duree_penalite_accordee_equipe1=duree_penalite_accordee_equipe1
+    
+    bouton_start_penalite_equipe1.hide()
+    timer_match.start(1000)
+    timer_match.timeout.connect(timer.penalite_accordee_equipe1)
+
+def start_penalite_equipe2():
+
+    
+    timer.duree_penalite_accordee_equipe2=duree_penalite_accordee_equipe2
+  
+    bouton_start_penalite_equipe2.hide()
+    timer_match.start(1000)
+    timer_match.timeout.connect(timer.penalite_accordee_equipe2)
+
+
+def fin_penalite_accordee_equipe1():
+    timer_match.timeout.disconnect(timer.penalite_accordee_equipe1)
+    if duree_penalite_accordee_equipe2 > 0:
+        bouton_start_penalite_equipe2.show()
+    else:
+        fin_penalite_accordee_equipe2()
+
+def fin_penalite_accordee_equipe2():
+    timer_match.timeout.disconnect(timer.penalite_accordee_equipe2)
+    if(numero_set<nombre_sets+1):
+        bouton_start_set.setText(f"START SET {numero_set}")
+        bouton_start_set.show()
 
 
 def choisir_2_sets():
@@ -38,10 +96,12 @@ def choisir_2_sets():
     bouton_2sets.hide()
     bouton_3sets.hide()
     texte_accueil.hide()
-
-    bouton_start.show()
+    
+    bouton_start_set.show()
     chrono.show()
     score.show()
+
+    
 
 
 def choisir_3_sets():
@@ -52,10 +112,11 @@ def choisir_3_sets():
     bouton_2sets.hide()
     bouton_3sets.hide()
     texte_accueil.hide()
-
-    bouton_start.show()
+   
+    bouton_start_set.show()
     chrono.show()
     score.show()
+
 
 chrono = QLabel(interface)
 chrono.setAlignment(Qt.AlignCenter)
@@ -68,8 +129,14 @@ chrono.hide()
 bouton_quitter = QPushButton("Quitter", interface)
 
 
-bouton_start = QPushButton(f"START SET {numero_set}", interface)
-bouton_start.hide()
+bouton_start_set = QPushButton(f"START SET {numero_set}", interface)
+bouton_start_set.hide()
+
+bouton_start_penalite_equipe1 = QPushButton(f"START PENALITE ACCORDEE A L'EQUIPE 1", interface)
+bouton_start_penalite_equipe1.hide()
+
+bouton_start_penalite_equipe2 = QPushButton(f"START PENALITE ACCORDEE A L'EQUIPE 2", interface)
+bouton_start_penalite_equipe2.hide()
 
 bouton_2sets = QPushButton("2 sets", interface)
 bouton_3sets = QPushButton("3 sets", interface)
@@ -135,15 +202,37 @@ texte_accueil.setGeometry(
 )
 
 
-# Bouton START
-bouton_start.setGeometry(
+# Bouton START_SET
+bouton_start_set.setGeometry(
     (largeur_ecran // 2) - largeur_ecran // 10,
     (hauteur_ecran // 2) - hauteur_ecran // 10,
     largeur_ecran // 10,
     hauteur_ecran // 10
 )
+
+bouton_start_set.clicked.connect(start_set)
+
+# Bouton START_PENALITE1
+bouton_start_penalite_equipe1.setGeometry(
+    (largeur_ecran // 2) - largeur_ecran // 6,
+    (hauteur_ecran // 2) - hauteur_ecran // 8,
+    largeur_ecran // 6,
+    hauteur_ecran // 8
+)
+bouton_start_penalite_equipe1.clicked.connect(start_penalite_equipe1)
+
+# Bouton START_PENALITE2
+bouton_start_penalite_equipe2.setGeometry(
+    (largeur_ecran // 2) - largeur_ecran // 6,
+    (hauteur_ecran // 2) - hauteur_ecran // 8,
+    largeur_ecran // 6,
+    hauteur_ecran // 8
+)
+bouton_start_penalite_equipe2.clicked.connect(start_penalite_equipe2)
+
+
+
 bouton_quitter.raise_()
-bouton_start.clicked.connect(start_set)
 
 score.setGeometry(
     largeur_ecran // 2,
@@ -169,16 +258,23 @@ timer_match = QTimer()
 
 timer = Timer(
     duree_match,
-    duree_penalite_accordee_equipe1,
-    duree_penalite_accordee_equipe2,
     chrono,
-    timer_match
+    timer_match,
+    fin_set,
+    fin_penalite_accordee_equipe1,
+    fin_penalite_accordee_equipe2
+
 )
 
 points = Score(score1, score2, score)
 points.update_score()
 
 
-timer_match.timeout.connect(timer.update_timer_match)
+
+
+
+
+
+
 
 sys.exit(app.exec())
--- timer.py
+++ timer.py
@@ -8,35 +8,47 @@ from PySide6.QtWidgets import * #importe tous les widgets
 class Timer(QWidget):
     
     
-    def __init__(self, duree_match, duree_penalite_accordee_equipe1,duree_penalite_accordee_equipe2 , chrono, timer_match):
+    def __init__(self, duree_match, chrono, timer_match, fin_set, fin_penalite_accordee_equipe1,fin_penalite_accordee_equipe2):
         super().__init__()
 
         self.duree_match = duree_match
         self.chrono = chrono
         self.timer_match = timer_match
-        self.duree_penalite_accordee_equipe1 = duree_penalite_accordee_equipe1
-        self.duree_penalite_accordee_equipe2 = duree_penalite_accordee_equipe2
         
-        self.mode="match"
+        self.fin_set = fin_set
+        self.fin_penalite_accordee_equipe1=fin_penalite_accordee_equipe1
+        self.fin_penalite_accordee_equipe2=fin_penalite_accordee_equipe2
+
+        self.duree_match_initiale = duree_match # utile pour ne pas repartir au set suivant avec duree_match=0
+
+        self.duree_penalite_accordee_equipe1 = 0
+        self.duree_penalite_accordee_equipe2 = 0 
+    
 
     def update_timer_match(self):
         
-        if self.mode=="match":
-            self.duree_match -= 1
+        self.duree_match -= 1
 
-            minutes = self.duree_match//60 # // division entière, sans virgule
-            secondes = self.duree_match % 60
+        minutes = self.duree_match//60 # // division entière, sans virgule
+        secondes = self.duree_match % 60
 
-            self.chrono.setStyleSheet("color:white; font-size:40px; border:2px solid white;")
-            self.chrono.setText(f"{minutes}:{secondes}")
+        self.chrono.setStyleSheet("color:white; font-size:40px; border:2px solid white;")
+        self.chrono.setText(f"{minutes}:{secondes}")
         
-            if self.duree_match == 0:   # si la fin du temps de match atteint
-                print("Temps match écoulé")
-                self.mode="penalite_accordee_equipe1"
+        if self.duree_match <= 0 :   # si la fin du temps de match atteint
+            self.duree_match = 0
+            print("Temps match écoulé")
+            self.stop_match()
+            self.fin_set() 
 
-
-        elif self.mode=="penalite_accordee_equipe1":  # on met elif à la palce de if car cela permet de passer à ce bloc si le premier if est faux
-        # sans le elif, quand on passe en mode "penalite" on perd une seconde car sur le même "tic" d'horloge on va rentrer dans la partie penalité
+    def penalite_accordee_equipe1 (self):
+            if self.duree_penalite_accordee_equipe1 <= 0 :   # si fin du temps de pénalités atteint
+                self.duree_penalite_accordee_equipe1 = 0
+                self.stop_match()   # on arrête le timer
+                print("Temps pénalité accordé à l'équipe 1 écoulé")
+                self.fin_penalite_accordee_equipe1()
+                return
+            
             self.duree_penalite_accordee_equipe1 -= 1
             
             minutes = self.duree_penalite_accordee_equipe1//60 # // division entière, sans virgule
@@ -45,6 +57,30 @@ class Timer(QWidget):
             self.chrono.setStyleSheet("color: red; font-size: 40px; border:2px solid white;")
             self.chrono.setText(f"{minutes}:{secondes}")
             
-            if self.duree_penalite_accordee_equipe1 == 0:   # si fin du temps de pénalités atteint
-                self.timer_match.stop()   # on arrête le timer
-                print("Temps pénalité accordé à l'équipe 1 écoulé")
+            
+
+    def penalite_accordee_equipe2 (self):
+         
+        if self.duree_penalite_accordee_equipe2 <= 0:   # si fin du temps de pénalités atteint
+            self.duree_penalite_accordee_equipe2 = 0
+            self.stop_match()   # on arrête le timer
+            print("Temps pénalité accordé à l'équipe 2 écoulé")
+            self.fin_penalite_accordee_equipe2()
+            return
+
+        self.duree_penalite_accordee_equipe2 -= 1
+            
+        minutes = self.duree_penalite_accordee_equipe2//60 # // division entière, sans virgule
+        secondes = self.duree_penalite_accordee_equipe2 % 60
+
+        self.chrono.setStyleSheet("color: red; font-size: 40px; border:2px solid white;")
+        self.chrono.setText(f"{minutes}:{secondes}")
+            
+        
+
+
+    def stop_match(self):
+        self.timer_match.stop()
+
+    def reset_match(self):
+        self.duree_match = self.duree_match_initiale
